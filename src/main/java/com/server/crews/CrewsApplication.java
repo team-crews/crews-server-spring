4f79ea10b399798308be7fcfa0105bcaa4f884ce
jongmee <ra123221@gmail.com>
2024-02-05 22:11:23 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/com/server/crews/CrewsApplication.java
@@ -0,0 +1,13 @@
+package com.server.crews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrewsApplication.class, args);
+    }
+
+}
