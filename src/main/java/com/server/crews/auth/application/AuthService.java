061e56f074cb4664029690691e72c802eeeb93e5
jongmee <ra123221@gmail.com>
2024-02-06 16:38:21 +0900

    feat: 지원서 양식 코드 생성시 토큰 발급 api 구현

---
+++ src/main/java/com/server/crews/auth/application/AuthService.java
@@ -0,0 +1,67 @@
+package com.server.crews.auth.application;
+
+import com.server.crews.application.repository.ApplicationRepository;
+import com.server.crews.auth.domain.Role;
+import com.server.crews.auth.dto.request.NewSecretCodeRequest;
+import com.server.crews.auth.dto.response.TokenResponse;
+import com.server.crews.global.exception.CrewsException;
+import com.server.crews.global.exception.ErrorCode;
+import com.server.crews.recruitment.domain.Recruitment;
+import com.server.crews.recruitment.repository.RecruitmentRepository;
+import jakarta.servlet.http.Cookie;
+import java.util.Objects;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RecruitmentRepository recruitmentRepository;
+    private final ApplicationRepository applicationRepository;
+
+    private final int refreshTokenValidityInSecond;
+
+    public AuthService(
+            final JwtTokenProvider jwtTokenProvider,
+            final RecruitmentRepository recruitmentRepository,
+            final ApplicationRepository applicationRepository,
+            @Value("${jwt.refresh-token-validity}") final int refreshTokenValidityInMilliseconds) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.recruitmentRepository = recruitmentRepository;
+        this.applicationRepository = applicationRepository;
+        this.refreshTokenValidityInSecond = refreshTokenValidityInMilliseconds / 1000;
+    }
+
+    public TokenResponse createRecruitmentCode(final NewSecretCodeRequest request) {
+        String code = request.code();
+        validateDuplicatedRecruitmentCode(code);
+        Recruitment recruitment = recruitmentRepository.save(new Recruitment(code));
+        String id = recruitment.getId();
+
+        String accessToken = jwtTokenProvider.createAccessToken(Role.ADMIN, id);
+        return new TokenResponse(id, accessToken);
+    }
+
+    public HttpHeaders createRefreshToken(Role role, String id) {
+        String refreshToken = jwtTokenProvider.createRefreshToken(role, id);
+
+        HttpHeaders headers = new HttpHeaders();
+        Cookie cookie = new Cookie("refreshToken", refreshToken);
+        cookie.setMaxAge(refreshTokenValidityInSecond);
+
+        headers.add(HttpHeaders.SET_COOKIE, cookie.toString());
+        return headers;
+    }
+
+    private void validateDuplicatedRecruitmentCode(final String code) {
+        Recruitment existing = recruitmentRepository.findBySecretCode(code).orElse(null);
+        validateDuplicated(existing);
+    }
+
+    private void validateDuplicated(Object existing) {
+        if(Objects.isNull(existing)) {
+            throw new CrewsException(ErrorCode.DUPLICATE_SECRET_CODE);
+        }
+    }
+}
---
+++ src/main/java/com/server/crews/auth/presentation/AuthController.java
@@ -0,0 +1,31 @@
+package com.server.crews.auth.presentation;
+
+import com.server.crews.auth.application.AuthService;
+import com.server.crews.auth.domain.Role;
+import com.server.crews.auth.dto.request.NewSecretCodeRequest;
+import com.server.crews.auth.dto.response.TokenResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/recruitment/secret-code")
+    @Operation(description = "지원서 양식에 대한 코드를 생성한다.")
+    public ResponseEntity<TokenResponse> createSecretCode(
+            @RequestBody final NewSecretCodeRequest request) {
+        TokenResponse tokenResponse = authService.createRecruitmentCode(request);
+        HttpHeaders headers = authService.createRefreshToken(Role.ADMIN, tokenResponse.id());
+        return ResponseEntity.status(HttpStatus.CREATED).headers(headers).body(tokenResponse);
+    }
+}
