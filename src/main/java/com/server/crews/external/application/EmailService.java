18cdf6f1eed017cf0c4873639d1f54cd89d58c42
jongmee <ra123221@gmail.com>
2024-02-11 16:50:08 +0900

    feat: 합/불 메일 보내는 mail service 구현

---
+++ src/main/java/com/server/crews/external/application/EmailService.java
@@ -0,0 +1,69 @@
+package com.server.crews.external.application;
+
+import com.server.crews.applicant.domain.Applicant;
+import com.server.crews.applicant.domain.Outcome;
+import com.server.crews.global.exception.CrewsException;
+import com.server.crews.recruitment.domain.Recruitment;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private static final String TITLE = "[Crews] 지원 결과 발표";
+    private  final JavaMailSender javaMailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    public void send(
+            final Applicant applicant,
+            final Outcome outcome,
+            final Recruitment recruitment) {
+        MimeMessagePreparator message = createMessage(applicant, outcome, recruitment);
+
+        try {
+            javaMailSender.send(message);
+        } catch (Exception e) {
+            throw new CrewsException(e.getMessage());
+        }
+    }
+
+    private MimeMessagePreparator createMessage(
+            final Applicant applicant,
+            final Outcome outcome,
+            final Recruitment recruitment) {
+        Context context = prepareVariables(applicant, recruitment);
+        String htmlName = determineHtml(outcome);
+
+        MimeMessagePreparator preparator = mimeMessage -> {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+            helper.setTo(applicant.getEmail());
+            helper.setSubject(TITLE);
+            String htmlContent = templateEngine.process(htmlName, context);
+            helper.setText(htmlContent, true);
+        };
+
+        return preparator;
+    }
+
+    private Context prepareVariables(final Applicant applicant, final Recruitment recruitment) {
+        Context context = new Context();
+        context.setVariables(Map.of(
+                "club", recruitment.getClubName(),
+                "name", applicant.getName(),
+                "recruitment", recruitment.getTitle()));
+        return context;
+    }
+
+    private String determineHtml(final Outcome outcome) {
+        if(outcome.equals(Outcome.PASS)) {
+            return "pass-email";
+        }
+        return "fail-email";
+    }
+}
