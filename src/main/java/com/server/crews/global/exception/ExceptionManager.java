fc0a9dca11a7de7d27cd5a9500bbafb80a253ade
jongmee <ra123221@gmail.com>
2024-08-10 16:50:30 +0900

    refactor: MethodArgumentNotValidException 핸들링 추가, 기본 제공되는 핸들러 상속 받기

--- src/main/java/com/server/crews/global/exception/ExceptionManager.java
+++ src/main/java/com/server/crews/global/exception/ExceptionManager.java
@@ -1,28 +1,45 @@
 package com.server.crews.global.exception;
 
+import java.util.Objects;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ExceptionManager {
-    @ExceptionHandler(CrewsException.class)
-    public ResponseEntity<ErrorDto> crewsExceptionHandler(CrewsException e){
-        return ResponseEntity.status(e.getHttpStatus()).body(new ErrorDto(e));
+public class ExceptionManager extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException e,
+                                                                          HttpHeaders headers, HttpStatusCode status,
+                                                                          WebRequest request) {
+        return ResponseEntity.status(ErrorCode.NO_PARAMETER.getHttpStatus())
+                .body(new ErrorDto(String.format(ErrorCode.NO_PARAMETER.getMessage(), e.getParameterName())));
     }
 
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<ErrorDto> handleMissingServletRequestParameter(MissingServletRequestParameterException e) {
-        return ResponseEntity.status(ErrorCode.NO_PARAMETER.getHttpStatus())
-                .body(new ErrorDto(ErrorCode.NO_PARAMETER.getHttpStatus().value(),
-                        String.format(ErrorCode.NO_PARAMETER.getMessage(), e.getParameterName())));
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers,
+                                                               HttpStatusCode status, WebRequest request) {
+        String errorMessage = Objects.requireNonNull(e.getBindingResult().getFieldError())
+                .getDefaultMessage();
+        return ResponseEntity.badRequest()
+                .body(new ErrorDto(errorMessage));
+    }
+
+    @ExceptionHandler(CrewsException.class)
+    public ResponseEntity<ErrorDto> crewsExceptionHandler(CrewsException e) {
+        return ResponseEntity.status(e.getHttpStatus()).body(new ErrorDto(e.getMessage()));
     }
 
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<ErrorDto> handleException(Throwable e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorDto(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()));
+                .body(new ErrorDto(e.getMessage()));
     }
 }
