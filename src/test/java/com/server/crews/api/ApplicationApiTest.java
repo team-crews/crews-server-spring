1fd3669c2055e3a12f8e21a902499847d9db7f76
jongmee <ra123221@gmail.com>
2024-08-18 14:36:31 +0900

    refactor: ApplicantApiTest 이름 변경

--- src/test/java/com/server/crews/api/ApplicationApiTest.java
+++ src/test/java/com/server/crews/api/ApplicationApiTest.java
@@ -1,26 +1,5 @@
 package com.server.crews.api;
 
-import com.server.crews.applicant.dto.request.AnswerSaveRequest;
-import com.server.crews.applicant.dto.request.ApplicationSaveRequest;
-import com.server.crews.applicant.dto.response.ApplicationDetailsResponse;
-import com.server.crews.applicant.dto.response.ApplicationsResponse;
-import com.server.crews.auth.dto.response.AccessTokenResponse;
-import com.server.crews.auth.presentation.AuthorizationExtractor;
-import com.server.crews.recruitment.dto.request.QuestionType;
-import com.server.crews.recruitment.dto.response.RecruitmentDetailsResponse;
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static com.server.crews.api.StatusCodeChecker.checkStatusCode200;
 import static com.server.crews.api.StatusCodeChecker.checkStatusCode201;
 import static com.server.crews.fixture.ApplicationFixture.DEFAULT_MAJOR;
 import static com.server.crews.fixture.ApplicationFixture.DEFAULT_NAME;
@@ -30,7 +9,24 @@ import static com.server.crews.fixture.UserFixture.TEST_EMAIL;
 import static com.server.crews.fixture.UserFixture.TEST_PASSWORD;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
-public class ApplicantApiTest extends ApiTest {
+import com.server.crews.applicant.dto.request.AnswerSaveRequest;
+import com.server.crews.applicant.dto.request.ApplicationSaveRequest;
+import com.server.crews.applicant.dto.response.ApplicationDetailsResponse;
+import com.server.crews.auth.dto.response.AccessTokenResponse;
+import com.server.crews.auth.presentation.AuthorizationExtractor;
+import com.server.crews.recruitment.dto.request.QuestionType;
+import com.server.crews.recruitment.dto.response.RecruitmentDetailsResponse;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+public class ApplicationApiTest extends ApiTest {
 
     @Test
     @DisplayName("지원자가 로그인하여 지원서를 처음으로 저장한다.")
@@ -38,17 +34,20 @@ public class ApplicantApiTest extends ApiTest {
         // given
         AccessTokenResponse adminTokenResponse = signUpAdmin(TEST_EMAIL, TEST_PASSWORD);
         RecruitmentDetailsResponse recruitmentDetailsResponse = createRecruitment(adminTokenResponse.accessToken());
-        AccessTokenResponse applicantTokenResponse = signUpApplicant(recruitmentDetailsResponse.code(), TEST_EMAIL, TEST_PASSWORD);
+        AccessTokenResponse applicantTokenResponse = signUpApplicant(recruitmentDetailsResponse.code(), TEST_EMAIL,
+                TEST_PASSWORD);
 
         List<AnswerSaveRequest> answerSaveRequests = List.of(
                 new AnswerSaveRequest(QuestionType.NARRATIVE, 2L, DEFAULT_NARRATIVE_ANSWER, List.of()),
                 new AnswerSaveRequest(QuestionType.SELECTIVE, 1L, null, List.of(1L, 2L)));
-        ApplicationSaveRequest applicationSaveRequest = new ApplicationSaveRequest(DEFAULT_STUDENT_NUMBER, DEFAULT_MAJOR, DEFAULT_NAME, answerSaveRequests);
+        ApplicationSaveRequest applicationSaveRequest = new ApplicationSaveRequest(DEFAULT_STUDENT_NUMBER,
+                DEFAULT_MAJOR, DEFAULT_NAME, answerSaveRequests);
 
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, AuthorizationExtractor.BEARER_TYPE + applicantTokenResponse.accessToken())
+                .header(HttpHeaders.AUTHORIZATION,
+                        AuthorizationExtractor.BEARER_TYPE + applicantTokenResponse.accessToken())
                 .body(applicationSaveRequest)
                 .when().post("/applications")
                 .then().log().all()
