2a1cb3054c602b198f5dd13b83be2d801c359fd3
jongmee <ra123221@gmail.com>
2024-08-25 17:48:08 +0900

    docs: api docs에 description 추가

--- src/test/java/com/server/crews/api/ApplicationApiDocuments.java
+++ src/test/java/com/server/crews/api/ApplicationApiDocuments.java
@@ -2,69 +2,69 @@ package com.server.crews.api;
 
 import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
-import org.springframework.restdocs.payload.RequestFieldsSnippet;
+import com.epages.restdocs.apispec.ResourceSnippetParametersBuilder;
 import org.springframework.restdocs.restassured.RestDocumentationFilter;
 
 public class ApplicationApiDocuments {
     private static final String APPLICATION_API = "application/";
 
     public static RestDocumentationFilter SAVE_APPLICATION_200_DOCUMENT() {
-        return document(APPLICATION_API + "지원서 저장", SAVE_APPLICATION_200_REQUEST_FIELDS());
-    }
-
-    public static RequestFieldsSnippet SAVE_APPLICATION_200_REQUEST_FIELDS() {
-        return requestFields(
-                fieldWithPath(".id")
-                        .description("지원서 id (최초 저장이 아닐 경우 필요함)")
-                        .optional(),
-                fieldWithPath(".studentNumber")
-                        .description("학번"),
-                fieldWithPath(".major")
-                        .description("전공"),
-                fieldWithPath(".name")
-                        .description("이름"),
-                fieldWithPath(".answers")
-                        .description("답변 목록")
-                        .optional(),
-                fieldWithPath(".answers[].answerId")
-                        .description("답변 id")
-                        .optional(),
-                fieldWithPath(".answers[].questionType")
-                        .description("질문 타입(NARRATIVE, SELECTIVE 중 하나)"),
-                fieldWithPath(".answers[].questionId")
-                        .description("질문 id"),
-                fieldWithPath(".answers[].content")
-                        .description("서술형 답안 (NARRATIVE 질문의 경우)")
-                        .optional(),
-                fieldWithPath(".answers[].choiceId")
-                        .description("선택지 id (SELECTIVE 질문의 경우)")
-                        .optional());
+        return document(APPLICATION_API + "지원서 저장",
+                new ResourceSnippetParametersBuilder().description("지원서를 저장한다.")
+                        .requestFields(
+                                fieldWithPath(".id")
+                                        .description("지원서 id (최초 저장이 아닐 경우 필요함)")
+                                        .optional(),
+                                fieldWithPath(".studentNumber")
+                                        .description("학번"),
+                                fieldWithPath(".major")
+                                        .description("전공"),
+                                fieldWithPath(".name")
+                                        .description("이름"),
+                                fieldWithPath(".answers")
+                                        .description("답변 목록")
+                                        .optional(),
+                                fieldWithPath(".answers[].answerId")
+                                        .description("답변 id")
+                                        .optional(),
+                                fieldWithPath(".answers[].questionType")
+                                        .description("질문 타입(NARRATIVE, SELECTIVE 중 하나)"),
+                                fieldWithPath(".answers[].questionId")
+                                        .description("질문 id"),
+                                fieldWithPath(".answers[].content")
+                                        .description("서술형 답안 (NARRATIVE 질문의 경우)")
+                                        .optional(),
+                                fieldWithPath(".answers[].choiceId")
+                                        .description("선택지 id (SELECTIVE 질문의 경우)")
+                                        .optional()));
     }
 
     public static RestDocumentationFilter SAVE_APPLICATION_404_DOCUMENT() {
-        return document(APPLICATION_API + "존재하지 않는 질문으로 지원서 저장");
+        return document(APPLICATION_API + "존재하지 않는 질문으로 지원서 저장",
+                new ResourceSnippetParametersBuilder().description("존재하지 않는 질문으로 지원서를 저장한다."));
     }
 
     public static RestDocumentationFilter SAVE_APPLICATION_400_DOCUMENT() {
-        return document(APPLICATION_API + "한 서술형 문항에 두 개 이상의 답변으로 지원서 저장");
+        return document(APPLICATION_API + "한 서술형 문항에 두 개 이상의 답변으로 지원서 저장",
+                new ResourceSnippetParametersBuilder().description("한 서술형 문항에 두 개 이상의 답변을 저장한다."));
     }
 
     public static RestDocumentationFilter GET_APPLICATION_200_DOCUMENT() {
         return document(APPLICATION_API + "지원서 상세 조회",
-                pathParameters(parameterWithName("application-id").description("지원서 id")));
+                new ResourceSnippetParametersBuilder().description("지원서 상세 정보를 조회한다.")
+                        .pathParameters(parameterWithName("application-id").description("지원서 id")));
     }
 
     public static RestDocumentationFilter GET_APPLICATIONS_200_DOCUMENT() {
-        return document(APPLICATION_API + "지원서 목록 조회");
+        return document(APPLICATION_API + "지원서 목록 조회",
+                new ResourceSnippetParametersBuilder().description("지원서 목록을 조회한다."));
     }
 
     public static RestDocumentationFilter EVALUATE_APPLICATIONS_200_DOCUMENT() {
         return document(APPLICATION_API + "지원서 평가",
-                requestFields(fieldWithPath(".passApplicationIds").description("합격 지원서 id 목록")));
+                new ResourceSnippetParametersBuilder().description("지원서를 평가한다.")
+                        .requestFields(fieldWithPath(".passApplicationIds").description("합격 지원서 id 목록")));
     }
-
 }
--- src/test/java/com/server/crews/api/AuthApiDocuments.java
+++ src/test/java/com/server/crews/api/AuthApiDocuments.java
@@ -1,13 +1,10 @@
 package com.server.crews.api;
 
 import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.document;
-import static org.springframework.restdocs.cookies.CookieDocumentation.cookieWithName;
-import static org.springframework.restdocs.cookies.CookieDocumentation.requestCookies;
-import static org.springframework.restdocs.cookies.CookieDocumentation.responseCookies;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 
+import com.epages.restdocs.apispec.ResourceSnippetParametersBuilder;
 import org.springframework.restdocs.restassured.RestDocumentationFilter;
 
 public class AuthApiDocuments {
@@ -15,60 +12,56 @@ public class AuthApiDocuments {
 
     public static RestDocumentationFilter LOGIN_ADMIN_200_DOCUMENT() {
         return document(AUTH_API + "동아리 운영진 로그인",
-                requestFields(
-                        fieldWithPath(".clubName")
-                                .description("동아리 이름"),
-                        fieldWithPath(".password")
-                                .description("비밀번호")),
-                responseFields(
-                        fieldWithPath(".adminId")
-                                .description("운영진 id"),
-                        fieldWithPath(".accessToken")
-                                .description("access token"),
-                        fieldWithPath(".recruitmentProgress")
-                                .description(
-                                        "모집 공고 상태 (READY: 모집 공고 작성 중, IN_PROGRESS: 모집 중, COMPLETION: 평가 중, ANNOUNCED: 이메일 전송 완료)"),
-                        fieldWithPath(".recruitmentId")
-                                .description("모집 공고 id (없다면 null)")
-                ),
-                responseCookies(
-                        cookieWithName("refreshToken")
-                                .description("리프레시 토큰")
-                ));
+                new ResourceSnippetParametersBuilder().description("동아리 운영진(admin)이 로그인한다.")
+                        .requestFields(
+                                fieldWithPath(".clubName")
+                                        .description("동아리 이름"),
+                                fieldWithPath(".password")
+                                        .description("비밀번호"))
+                        .responseFields(
+                                fieldWithPath(".adminId")
+                                        .description("운영진 id"),
+                                fieldWithPath(".accessToken")
+                                        .description("access token"),
+                                fieldWithPath(".recruitmentProgress")
+                                        .description(
+                                                "모집 공고 상태 (READY: 모집 공고 작성 중, IN_PROGRESS: 모집 중, COMPLETION: 평가 중, ANNOUNCED: 이메일 전송 완료)"),
+                                fieldWithPath(".recruitmentId")
+                                        .description("모집 공고 id (없다면 null)"))
+                        .responseHeaders(headerWithName("Cookie")
+                                .description("리프레시 토큰")));
     }
 
     public static RestDocumentationFilter LOGIN_APPLICANT_200_DOCUMENT() {
         return document(AUTH_API + "지원자 로그인",
-                requestFields(
-                        fieldWithPath(".recruitmentCode")
-                                .description("모집 공고 코드"),
-                        fieldWithPath(".email")
-                                .description("이메일"),
-                        fieldWithPath(".password")
-                                .description("비밀번호")),
-                responseFields(
-                        fieldWithPath(".applicantId")
-                                .description("지원자 id"),
-                        fieldWithPath(".accessToken")
-                                .description("access token"),
-                        fieldWithPath(".recruitmentProgress")
-                                .description("모집 공고 상태 (IN_PROGRESS: 모집 중, COMPLETION: 평가 중)"),
-                        fieldWithPath(".applicationId")
-                                .description("지원서 id (없다면 null)")
-                ),
-                responseCookies(
-                        cookieWithName("refreshToken")
-                                .description("리프레시 토큰")
-                ));
+                new ResourceSnippetParametersBuilder().description("지원자(applicant)가 로그인한다.")
+                        .requestFields(
+                                fieldWithPath(".recruitmentCode")
+                                        .description("모집 공고 코드"),
+                                fieldWithPath(".email")
+                                        .description("이메일"),
+                                fieldWithPath(".password")
+                                        .description("비밀번호"))
+                        .responseFields(
+                                fieldWithPath(".applicantId")
+                                        .description("지원자 id"),
+                                fieldWithPath(".accessToken")
+                                        .description("access token"),
+                                fieldWithPath(".recruitmentProgress")
+                                        .description("모집 공고 상태 (IN_PROGRESS: 모집 중, COMPLETION: 평가 중)"),
+                                fieldWithPath(".applicationId")
+                                        .description("지원서 id (없다면 null)")));
     }
 
     public static RestDocumentationFilter REFRESH_TOKEN_200_DOCUMENT() {
         return document(AUTH_API + "토큰 재발급",
-                requestCookies(cookieWithName("refreshToken")
-                        .description("리프레시 토큰")));
+                new ResourceSnippetParametersBuilder().description("access token을 재발급 받는다.")
+                        .requestHeaders(headerWithName("Cookie")
+                                .description("리프레시 토큰")));
     }
 
     public static RestDocumentationFilter AUTHORIZE_401_DOCUMENT() {
-        return document(AUTH_API + "인가 실패 예시");
+        return document(AUTH_API + "인가 실패 예시",
+                new ResourceSnippetParametersBuilder().description("(인가 실패 예시) 지원자 권한으로 지원자 목록을 조회한다"));
     }
 }
