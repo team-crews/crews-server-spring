c1b5bf5c229e9c59a5754b4db3d825fed10a005d
jongmee <ra123221@gmail.com>
2024-08-26 16:24:12 +0900

    refactor: 로그인 api에서 불필요한 반환값 제거

--- src/main/java/com/server/crews/auth/dto/response/ApplicantLoginResponse.java
+++ src/main/java/com/server/crews/auth/dto/response/ApplicantLoginResponse.java
@@ -1,11 +1,4 @@
 package com.server.crews.auth.dto.response;
 
-import com.server.crews.recruitment.domain.RecruitmentProgress;
-
-public record ApplicantLoginResponse(
-        Long applicantId,
-        String accessToken,
-        RecruitmentProgress recruitmentProgress,
-        Long applicationId
-) {
+public record ApplicantLoginResponse(Long applicantId, String accessToken, Long applicationId) {
 }
