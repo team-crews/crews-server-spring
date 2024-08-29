98b1cd8618b2e7611bc5e26bb0da252b5f66bf9c
jongmee <ra123221@gmail.com>
2024-08-30 00:33:16 +0900

    refactor: 불필요한 의존성 제거

--- src/main/java/com/server/crews/auth/application/AuthService.java
+++ src/main/java/com/server/crews/auth/application/AuthService.java
@@ -14,7 +14,6 @@ import com.server.crews.auth.repository.AdministratorRepository;
 import com.server.crews.auth.repository.ApplicantRepository;
 import com.server.crews.global.exception.CrewsException;
 import com.server.crews.global.exception.ErrorCode;
-import com.server.crews.recruitment.repository.RecruitmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,7 +23,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthService {
     private final AdministratorRepository administratorRepository;
     private final ApplicantRepository applicantRepository;
-    private final RecruitmentRepository recruitmentRepository;
     private final ApplicationRepository applicationRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
