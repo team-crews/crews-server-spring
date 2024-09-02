7a649e1bad7ccbe53ac5c602414ddaa57d4f755f
jongmee <ra123221@gmail.com>
2024-09-02 15:45:18 +0900

    refactor: ExceptionHandler 클래스 이름 변경

--- src/main/java/com/server/crews/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/server/crews/global/exception/GlobalExceptionHandler.java
@@ -7,12 +7,13 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException e,
@@ -31,12 +32,12 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
                 .body(new ErrorDto(errorMessage));
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(CrewsException.class)
+    @ExceptionHandler(CrewsException.class)
     public ResponseEntity<ErrorDto> crewsExceptionHandler(CrewsException e) {
         return ResponseEntity.status(e.getHttpStatus()).body(new ErrorDto(e.getMessage()));
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(Throwable.class)
+    @ExceptionHandler(Throwable.class)
     public ResponseEntity<ErrorDto> handleException(Throwable e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorDto(e.getMessage()));
