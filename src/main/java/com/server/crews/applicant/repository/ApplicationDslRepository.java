85bbd95c8f4cd6a1d982bb6c2c7d8bc700ec961e
jongmee <ra123221@gmail.com>
2024-09-02 16:20:22 +0900

    refactor: application query dsl repository 제거

--- src/main/java/com/server/crews/applicant/repository/ApplicationDslRepository.java
+++
@@ -1,8 +0,0 @@
-package com.server.crews.applicant.repository;
-
-import com.server.crews.applicant.domain.Application;
-import java.util.List;
-
-public interface ApplicationDslRepository {
-    List<Application> findAllWithApplicantByPublisherId(Long publisherId);
-}
