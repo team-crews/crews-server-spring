46fac0b1b558a2d15f060fb080b53d19dacacbe6
jongmee <ra123221@gmail.com>
2024-11-23 14:57:26 +0900

    refactor: 사용하지 않는 쿼리 메소드 제거

--- src/main/java/com/server/crews/recruitment/repository/NarrativeQuestionRepository.java
+++ src/main/java/com/server/crews/recruitment/repository/NarrativeQuestionRepository.java
@@ -1,10 +1,7 @@
 package com.server.crews.recruitment.repository;
 
 import com.server.crews.recruitment.domain.NarrativeQuestion;
-import com.server.crews.recruitment.domain.Section;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NarrativeQuestionRepository extends JpaRepository<NarrativeQuestion, Long> {
-    List<NarrativeQuestion> findAllBySectionIn(List<Section> sections);
 }
