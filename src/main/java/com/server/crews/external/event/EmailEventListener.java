7c3dcd408251751add774643600abe913e22785a
jongmee <ra123221@gmail.com>
2024-12-01 19:33:19 +0900

    feat: email 전송 batch 처리, 비동기 메소드 스레드 풀 변경

--- src/main/java/com/server/crews/external/event/EmailEventListener.java
+++ src/main/java/com/server/crews/external/event/EmailEventListener.java
@@ -6,7 +6,6 @@ import com.server.crews.external.application.EmailService;
 import com.server.crews.recruitment.domain.Recruitment;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -16,12 +15,13 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class EmailEventListener {
     private final EmailService emailService;
 
-    @Async
     @TransactionalEventListener(value = OutcomeDeterminedEvent.class, phase = TransactionPhase.AFTER_COMMIT)
     public void sendEmailToApplicants(OutcomeDeterminedEvent event) {
         Recruitment recruitment = event.recruitment();
         List<Application> applications = event.applications();
 
-        applications.forEach(application -> emailService.send(application, recruitment));
+        int middleIndex = applications.size() / 2;
+        emailService.sendBatch(applications.subList(0, middleIndex), recruitment);
+        emailService.sendBatch(applications.subList(middleIndex, applications.size()), recruitment);
     }
 }
--- src/main/java/com/server/crews/global/config/AsyncConfiguration.java
+++ src/main/java/com/server/crews/global/config/AsyncConfiguration.java
@@ -1,12 +1,15 @@
 package com.server.crews.global.config;
 
 import com.server.crews.global.exception.AsyncExceptionHandler;
+import java.util.concurrent.Executor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @Configuration
@@ -17,5 +20,16 @@ public class AsyncConfiguration implements AsyncConfigurer {
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
         return new AsyncExceptionHandler(applicationEventPublisher);
+    }
+
+    @Bean(name = "emailThreadPoolTaskExecutor")
+    public Executor emailThreadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(6);
+        threadPoolTaskExecutor.setMaxPoolSize(6);
+        threadPoolTaskExecutor.setQueueCapacity(300);
+        threadPoolTaskExecutor.setThreadNamePrefix("Async-Email-");
+        threadPoolTaskExecutor.initialize();
+        return threadPoolTaskExecutor;
     }
 }
